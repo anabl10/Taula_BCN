ff0d4c6d5647c4d7d588fe229ed56d01adf63e24
Mireia Boneta Cami <mireia.boneta.cami@pwc.com>
2022-07-20 17:56:22 +0200

    Scraping Boletines

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # MIREIA una función que haga un super scrapping de Andalucía boletines
+print("Ana te odio, eres la peor jefa que podemos tener, DIMITE YA #AnaVeteYa")
 
-# JAVI una funciónn que cuente palabras
+# JAVI una función que cuente palabras
 
 # JOSE una función que calcule pi
